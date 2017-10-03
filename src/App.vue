507d2a2c73e02fa31f62615dc63ea48907c5e1a6
Guy Geva <guygeva2311@gmail.com>
2017-10-03 20:18:59 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <svg width="400" height="110">
+    <nuc @mousedown="dragChild" type="A" :RY="false"/>
+  </svg>
+</template>
+
+<script>
+import nuc from './components/base'
+
+export default {
+  name: 'app',
+  components: {
+    nuc
+  },
+  methods: {
+    dragChild (e) {
+      console.log(e)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/base.vue
@@ -0,0 +1,43 @@
+<template>
+    <rect @mouseup="mouseUp()" @mousedown="mouseDown()" :class="'nuc-' + computedType" width="35" height="35" rx="5" ry="5" style="stroke-width:1;stroke:rgb(0,0,0)" />
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+    }
+  },
+  props: ['type', 'RY'],
+  computed: {
+    computedType () {
+      return this.RY ? this.type === 'G' || this.type === 'A' || this.type === 'R' ? 'R' : 'Y' : this.type
+    }
+  },
+  methods: {
+    mouseDown () {
+      this.$emit('mousedown', this)
+    },
+    mouseUp () {
+      this.$emit('mouseup', this)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  * {
+    cursor: pointer;
+  }
+  .nuc-A {
+    fill:rgb(255,255,0);    
+  }
+  .nuc-A:hover {
+    fill:rgb(200,200,0);
+  }
+  .nuc-R {
+    fill:#ffa500;  
+  }
+</style>
