24c61ee8d3f4493e95af4ade7982bfc994e4b315
Guy Geva <guygeva2311@gmail.com>
2017-12-05 15:57:49 +0200

    start of communication with eterna

---
+++ src/components/Menu.vue
@@ -0,0 +1,17 @@
+<template>
+  <table>
+    <tr v-for="lane in labes">
+
+    </tr>
+  </table>
+</template>
+
+<script>
+  export default {
+    porps: ['lanes'],//lanes[i] = {name, eternaPos, eternaDirection?, sequence}
+  }
+</script>
+
+<style>
+
+</style>
